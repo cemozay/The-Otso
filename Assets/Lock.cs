41fa37fd403caabdbf164623e211b4c4d33e3c67
alpagi <Alpereniga@gmail.com>
2023-09-30 00:53:44 +0300

    Envanters

--- Assets/Lock.cs
+++ Assets/Lock.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class sos : MonoBehaviour
+public class Lock : MonoBehaviour
 {
     void Update()
     {
--- Assets/ses.cs
+++ Assets/ses.cs
@@ -8,8 +8,10 @@ using DG.Tweening;
 public class ses : MonoBehaviour, IInteractable
 {   
     private Vector3 eski;
+    private Quaternion eskirotation;
     private Vector3 yeniPosition;
     public GameObject envanter;
+    public GameObject Lamba;
     public GameObject cam;
     public GameObject carklar;
     public GameObject kilidi;
@@ -26,18 +28,23 @@ public class ses : MonoBehaviour, IInteractable
 
         yeniPosition = cam.transform.position;
         
+        eskirotation = carklar.transform.rotation;
     }
     public void Interact()
     {
+        Lamba.SetActive(true);
         envanter.SetActive(false);
-        cam.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+        cam.transform.rotation = Quaternion.Euler(0f, 270f, 0f);
         carklar.transform.position = cam.transform.position;
         Vector3 yeniPozisyon = transform.position;
-        yeniPozisyon.x += 0.373015f; 
+        yeniPozisyon.x -= 0.65f;
+        yeniPozisyon.y -= 0.05f;
         transform.position = yeniPozisyon;
 
+        carklar.transform.rotation = Quaternion.Euler(0f, 0f, 156f);
 
-       
+
+
 
     }
     private void Update()
@@ -47,11 +54,14 @@ public class ses : MonoBehaviour, IInteractable
             envanter.SetActive(true);
             cam.transform.rotation = Quaternion.Euler(45f, 135f, 0f);
             transform.position = eski;
+            gameManager.isInteracted = false;
+            carklar.transform.rotation = eskirotation;
+
         }
-        if (Mathf.Approximately(cark1.rotation.eulerAngles.z,18.04914f) &&
-        Mathf.Approximately(cark2.rotation.eulerAngles.z, 90.04914f) &&
-        Mathf.Approximately(cark3.rotation.eulerAngles.z, 54.04914f) &&
-        Mathf.Approximately(cark4.rotation.eulerAngles.z, 306.0491f))
+        if (Mathf.Approximately(cark1.rotation.eulerAngles.z,17.999999f) &&
+        Mathf.Approximately(cark2.rotation.eulerAngles.z, 90.00002f) &&
+        Mathf.Approximately(cark3.rotation.eulerAngles.z, 54.00001f) && 
+        Mathf.Approximately(cark4.rotation.eulerAngles.z, 306f))
         {
 
             kilidi.SetActive(false);
@@ -59,10 +69,11 @@ public class ses : MonoBehaviour, IInteractable
             Mektubunimage.GetComponent<Button>().enabled = true;
             Image ýmage = Mektubunimage.GetComponent<Image>();
             ýmage.color = Color.white;
-
+            gameManager.isInteracted = false;
             Destroy(carklar);
-            cam.transform.rotation = Quaternion.Euler(45f, 135f, 0f);
+            cam.transform.rotation = Quaternion.Euler(50f, 140f, 5.5f);
             Mektubu.SetActive(true);
+
 
 
         }
